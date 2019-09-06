9ab0f759dd9d6476b81acc1ad62f4cf4f6d30669
Marco Melilli <marcomelilli92@hotmail.it>
2019-09-06 18:01:41 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,11 @@
 export default {
     "db": {
-        "user": "root",
-        "pass": "example",
-        "host": "mongo",
+        "user": null,
+        "pass": null,
+        "host": "localhost",
+        "port": "27017",
         "database": "testdb",
-        "authSource": "admin"
+        "authSource": null
     },
     "host": {
         "url": "<server-url>",
